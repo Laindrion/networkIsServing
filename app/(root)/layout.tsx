66a85ad87db23f120a5e2b0d0f4c76c9d90cc0cb
Lain <igencap@gmail.com>
2024-10-04 09:28:43 +0500

    first

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -4,9 +4,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-   <main>
-        SIDEBAR
-        {children}
-   </main>
+    <main>
+      SIDEBAR
+      {children}
+    </main>
   );
 }
