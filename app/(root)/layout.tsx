b004894e1e83836c9b916c33cbcb3e0bf5589b62
Lain <igencap@gmail.com>
2025-01-27 17:37:47 +0500

    stopped in shadcn mobile navbar/sidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,14 +1,32 @@
-
+import Sidebar from "@/components/ui/Sidebar";
+import Image from "next/image";
+import MobileNavbar from "@/components/ui/MobileNavbar";
 
 export default function RootLayout({
     children,
 }: Readonly<{
     children: React.ReactNode;
 }>) {
+    const loggedIn = {
+        firstName: "Oh Wow",
+        lastName: "That's pizza"
+    }
     return (
-        <main>
-            SIDEBAR
-            {children}
+        <main className="flex h-screen w-full font-inter">
+            <Sidebar
+                user={loggedIn}
+            />
+            <div className="flex size-full flex-col">
+                <div className="root-layout">
+                    <Image src="/icons/logo.svg"
+                        width={30} height={30}
+                        alt="logo" />
+                    <div>
+                        <MobileNavbar user={loggedIn} />
+                    </div>
+                </div>
+                {children}
+            </div>
         </main>
     );
 }
---
+++ components/ui/AnimatedCounter.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import CountUp from "react-countup"
+
+const AnimatedCounter = ({ amount }: { amount: number }) => {
+    return (
+        <p>
+            <CountUp decimals={2} decimal="," prefix="$" end={amount} />
+        </p>
+    )
+}
+
+export default AnimatedCounter
