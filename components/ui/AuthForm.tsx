20a2d94e9c29125d30b8e3f4a34ae3e409e6cdf6
Lain <igencap@gmail.com>
2025-02-04 17:45:14 +0500

    did

--- components/ui/AuthForm.tsx
+++ components/ui/AuthForm.tsx
@@ -23,9 +23,12 @@ import { Input } from "@/components/ui/input"
 import CustomInput from './CustomInput'
 import { authFormSchema } from '@/lib/utils'
 import { Loader2 } from 'lucide-react'
+import { useRouter } from 'next/navigation'
+import { signIn, signUp } from '@/lib/actions/user.actions.ts'
 
 
 const AuthForm = ({ type }: { type: string }) => {
+   const router = useRouter();
    const [user, setUser] = useState(null);
    const [isLoading, setIsLoading] = useState()
 
@@ -52,11 +55,16 @@ const AuthForm = ({ type }: { type: string }) => {
          if (type === "sign-up") {
             const newUser = await signUp(data);
 
-            setUser
+            setUser(newUser);
          }
 
          if (type === "sign-in") {
+            const response = await signIn({
+               email: data.email,
+               password: data.password
+            })
 
+            if (response) router.push("/")
          }
       } catch (error) {
          console.log(error)
@@ -217,7 +225,6 @@ const AuthForm = ({ type }: { type: string }) => {
                         type === "sign-in" ? "Sign Up" : "Sign In"
                      }
                   </Link>
-
                </footer>
             </>
          )}
