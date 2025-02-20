6dd3391fa87e7efb76a2b8362319921bdb3a7cb2
Lain <igencap@gmail.com>
2025-02-20 12:14:55 +0500

    did

--- components/ui/AuthForm.tsx
+++ components/ui/AuthForm.tsx
@@ -4,21 +4,15 @@ import React, { useState } from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
 
-import { set, z } from "zod"
+import { /* set, */ z } from "zod"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 
 import { Button } from "@/components/ui/button"
 import {
    Form,
-   FormControl,
-   FormDescription,
-   FormField,
-   FormItem,
-   FormLabel,
-   FormMessage,
 } from "@/components/ui/form"
-import { Input } from "@/components/ui/input"
+import { /* Input */ } from "@/components/ui/input"
 
 import CustomInput from './CustomInput'
 import { authFormSchema } from '@/lib/utils'
