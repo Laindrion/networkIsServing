b1b0592e8485162d6e5f79a130b871a7e43e2896
Owtp <igencap@gmail.com>
2025-03-03 19:07:57 +0500

    did

--- components/ui/AuthForm.tsx
+++ components/ui/AuthForm.tsx
@@ -18,7 +18,6 @@ import CustomInput from './CustomInput'
 import { authFormSchema } from '@/lib/utils'
 import { Loader2 } from 'lucide-react'
 import { useRouter } from 'next/navigation'
-import { signIn, signUp } from '@/lib/actions/user.actions'
 import { getLoggedInUser, signIn, signUp } from '../../lib/actions/user.actions'
 import { createAdminClient } from '@/lib/appwrite'
 
