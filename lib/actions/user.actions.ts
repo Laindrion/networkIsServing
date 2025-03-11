0fec3515660dee5edb5a31943f6efc943cc8f925
Lain <igencap@gmail.com>
2025-03-11 17:06:44 +0500

    did

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -71,8 +71,9 @@ export const logoutAccount = async () => {
       cookies().delete("appwrite-session");
 
       await account.deleteSession("current");
+
    } catch (error) {
-      console.log(error)
+      console.log(error);
       return null;
    }
 }
