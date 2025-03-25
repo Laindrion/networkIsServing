2e4deca207c304ca5a90084d7291187ff2015ae2
Lain <igencap@gmail.com>
2025-03-25 18:20:52 +0500

    did

--- lib/plaid.ts
+++ lib/plaid.ts
@@ -4,7 +4,7 @@ import { Configuration, PlaidApi, PlaidEnvironments } from "plaid"
 const configuration = new Configuration({
    basePath: PlaidEnvironments.sandbox,
    baseOptions: {
-      headers:{
+      headers: {
          "PLAID-CLIENT-ID": process.env.PLAID_CLIENT_ID,
          "PLAID-SECRET": process.env.PLAID_SECRET
       }
