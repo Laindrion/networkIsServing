bb0d22fd82a37934a172d992f06efd26cd0a4e9d
Lain <igencap@gmail.com>
2025-04-01 17:41:44 +0500

    did

--- components/RecentTransactions.tsx
+++ components/RecentTransactions.tsx
@@ -5,62 +5,61 @@ import { BankTabItem } from './BankTabItem'
 import BankInfo from './BankInfo'
 import TransactionsTable from './TransactionsTable'
 
-
 const RecentTransactions = (
-    {
-        accounts,
-        transactions = [],
-        appwriteItemId,
-        page = 1
-    }: RecentTransactionsProps) => {
-    return (
-        <section className="recent-transactions">
-            <header className="flex item-center justify-between">
-                <h2 className="recent-transactions-label">
-                    Recent transactions
-                </h2>
+   {
+      accounts,
+      transactions = [],
+      appwriteItemId,
+      page = 1
+   }: RecentTransactionsProps) => {
+   return (
+      <section className="recent-transactions">
+         <header className="flex item-center justify-between">
+            <h2 className="recent-transactions-label">
+               Recent transactions
+            </h2>
 
-                <Link href={`/transaction-history/?id=${appwriteItemId}`} className="view-all-btn">
-                    View all
-                </Link>
-            </header>
+            <Link href={`/transaction-history/?id=${appwriteItemId}`} className="view-all-btn">
+               View all
+            </Link>
+         </header>
 
-            <Tabs defaultValue={appwriteItemId} className="w-full">
-                <TabsList className="recent-transactions-tablist">
-                    {accounts.map((account: Account) => (
-                        <TabsTrigger key={account.id} value={account.appwriteItemId}>
-                            <BankTabItem
-                                key={account.id}
-                                account={account}
-                                appwriteItemId={appwriteItemId}
-                            />
-                        </TabsTrigger>
-                    ))}
-                </TabsList>
-
-                {accounts.map((account: Account) => (
-                    <TabsContent
-                        value={account.appwriteItemId}
+         <Tabs defaultValue={appwriteItemId} className="w-full">
+            <TabsList className="recent-transactions-tablist">
+               {accounts.map((account: Account) => (
+                  <TabsTrigger key={account.id} value={account.appwriteItemId}>
+                     <BankTabItem
                         key={account.id}
-                        className="space-y-4"
-                    >
+                        account={account}
+                        appwriteItemId={appwriteItemId}
+                     />
+                  </TabsTrigger>
+               ))}
+            </TabsList>
 
-                        <BankInfo
-                            account={account}
-                            appwriteItemId={appwriteItemId}
-                            type="full"
-                        />
+            {accounts.map((account: Account) => (
+               <TabsContent
+                  value={account.appwriteItemId}
+                  key={account.id}
+                  className="space-y-4"
+               >
 
-                        <TransactionsTable
-                            transactions={transactions}
-                        />
+                  <BankInfo
+                     account={account}
+                     appwriteItemId={appwriteItemId}
+                     type="full"
+                  />
 
-                    </TabsContent>
-                ))}
-            </Tabs>
+                  <TransactionsTable
+                     transactions={transactions}
+                  />
 
-        </section>
-    )
+               </TabsContent>
+            ))}
+         </Tabs>
+
+      </section>
+   )
 }
 
 export default RecentTransactions
--- components/TransactionsTable.tsx
+++ components/TransactionsTable.tsx
@@ -1,51 +1,51 @@
 import {
-    Table,
-    TableBody,
-    TableCaption,
-    TableCell,
-    TableHead,
-    TableHeader,
-    TableRow,
+   Table,
+   TableBody,
+   TableCaption,
+   TableCell,
+   TableHead,
+   TableHeader,
+   TableRow,
 } from "@/components/ui/table"
 import { formatAmount, getTransactionStatus } from "@/lib/utils"
 
 
 const TransactionsTable = ({ transaction }: TransactionTableProps) => {
-    return (
-        <Table>
-            <TableHeader className="bg-[#f9fafb]">
-                <TableRow>
-                    <TableHead className="px-2">Transaction</TableHead>
-                    <TableHead className="px-2">Amount</TableHead>
-                    <TableHead className="px-2">Status</TableHead>
-                    <TableHead className="px-2">Date</TableHead>
-                    <TableHead className="px-2 max-md:hidden">Channel</TableHead>
-                    <TableHead className="px-2 max-md:hidden">Category</TableHead>
-                </TableRow>
-            </TableHeader>
-            <TableBody>
-                {transaction.map((t: Transaction) => {
-                    const status = getTransactionStatus(new Date(t.date))
-                    const amount = formatAmount(t.amount)
+   return (
+      <Table>
+         <TableHeader className="bg-[#f9fafb]">
+            <TableRow>
+               <TableHead className="px-2">Transaction</TableHead>
+               <TableHead className="px-2">Amount</TableHead>
+               <TableHead className="px-2">Status</TableHead>
+               <TableHead className="px-2">Date</TableHead>
+               <TableHead className="px-2 max-md:hidden">Channel</TableHead>
+               <TableHead className="px-2 max-md:hidden">Category</TableHead>
+            </TableRow>
+         </TableHeader>
+         <TableBody>
+            {transaction.map((t: Transaction) => {
+               const status = getTransactionStatus(new Date(t.date))
+               const amount = formatAmount(t.amount)
 
-                    const isDebit = t.type === "debit";
-                    const isCredit = t.type === "credit";
+               const isDebit = t.type === "debit";
+               const isCredit = t.type === "credit";
 
-                    return (
-                      <TableRow key={t.id}>
-                        <TableCell className="px-2">
-                            <div>
-                                <h1>
-                                    {t.name}
-                                </h1>
-                            </div>
-                        </TableCell>
-                      </TableRow>
-                    )
-                })}
-            </TableBody>
-        </Table>
-    )
+               return (
+                  <TableRow key={t.id}>
+                     <TableCell className="px-2">
+                        <div>
+                           <h1>
+                              {t.name}
+                           </h1>
+                        </div>
+                     </TableCell>
+                  </TableRow>
+               )
+            })}
+         </TableBody>
+      </Table>
+   )
 }
 
 export default TransactionsTable
